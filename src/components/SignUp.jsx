5de36f3899728bdc3b48a756afeb4874c9652c98
CHUL CHIVORN <chivornchul2@gmail.com>
2025-11-19 18:03:22 +0700

    started doing signup with email

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -2,16 +2,31 @@
 // import google_icon from '../assets/icons/google-icon.svg';
 // import github_icon from '../assets/icons/github-icon.svg';
 // import x_twitter_icon from '../assets/icons/x-twitter-icon.svg';
+import { useState } from 'react';
+import useSignUpWithEmailAndPassword from '../hooks/useSignUpWithEmailAndPassword';
 import { ASSETS } from '../utils/assets';
 import { Link } from 'react-router-dom';
 
 const SignUp = () => {
 	const { facebookIcon, googleIcon, githubIcon, xTwitterIcon } = ASSETS;
+
+	const [email, setEmail] = useState('');
+	const {
+		signUp: emailSignUp,
+		isLoading: emailIsLoading,
+		error: emailError,
+	} = useSignUpWithEmailAndPassword;
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		try {
+			await emailSignUp(email, password);
+		} catch (error) {}
+	};
 	return (
 		<div className='signup-form'>
 			<h1>Sign Up</h1>
 			<span>Let's Start Your Wonderful Journey With TrackMate</span>
-			<form action=''>
+			<form onSubmit={handleSubmit}>
 				<ul>
 					<label htmlFor='username'>Username</label>
 					<li>
@@ -38,6 +53,9 @@ const SignUp = () => {
 							<span> Privacy Policy</span>
 						</span>
 					</li>
+					<button t ype='submit'>
+						Sign Up
+					</button>
 				</ul>
 			</form>
 			<div className='signup-with'>
--- src/hooks/useSignUpWithEmailAndPassword.js
+++ src/hooks/useSignUpWithEmailAndPassword.js
@@ -1,10 +1,33 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
+import { auth } from '../components/firebase';
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+
 const useSignUpWithEmailAndPassword = () => {
-	const [error, isError] = useState(null);
+	const [error, setError] = useState(null);
 	const [isLoading, setIsLoading] = useState(false);
 	const navigate = useNavigate();
+
+	const signUp = async (username, email, password) => {
+		setIsLoading(true);
+		setError(null);
+
+		try {
+			const userCredential = await createUserWithEmailAndPassword(
+				auth,
+				email,
+				password
+			);
+			console.log(userCredential.user);
+			navigate('/');
+			return userCredential.user;
+		} catch (error) {
+			setError(error.message);
+		} finally {
+			setIsLoading(false);
+		}
+	};
 
 	return;
 };
