13c28bea1cf1b18f4081d8d76926e94110799115
CHUL CHIVORN <chivornchul2@gmail.com>
2026-01-29 10:08:29 +0700

    - Made navbar responsive

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -4,39 +4,74 @@ import { ASSETS } from '../../utils/assets';
 
 import './Navigation.css';
 import { useAuth } from '../../auth/AuthProvider';
-import { button, li } from 'framer-motion/client';
+// import { button, li } from 'framer-motion/client';
 import { signOut } from 'firebase/auth';
 import { auth } from '../firebase';
+import { useState } from 'react';
+import { ul } from 'framer-motion/client';
 
 const Navigation = () => {
 	const { watchLogo } = ASSETS;
 	const { user, isLoading, isAdmin } = useAuth();
+
+	const [moreOpen, setMoreOpen] = useState(false);
 	return (
 		<div className='navbar'>
-			<div>
-				{/* <img className='watch-logo' src={watchLogo} alt='Watch Logo' /> */}
+			<div className='brand-wrapper'>
 				<span className='brand'>CHIRON's</span>
 			</div>
-			<ul>
-				<li>Home</li>
-				<li>Shop</li>
-				<li>Cart</li>
-				<li>About Us</li>
-				<li>
-					{user ? (
-						<button onClick={() => signOut(auth)}>Sign Out</button>
-					) : (
-						<Link className='navbar-items' to='signin'>
-							Sign In
-						</Link>
-					)}
-				</li>
-				{isAdmin && (
-					<li>
-						<Link className='navbar-items'>Admin</Link>
+			<div className='nav-items-wrapper'>
+				{/* <img className='watch-logo' src={watchLogo} alt='Watch Logo' /> */}
+				<ul className='nav-items'>
+					<li>Home</li>
+					<li>Shop</li>
+					<li>Cart</li>
+					<li className='hideSm'>About Us</li>
+					<li className='hideSm'>
+						{user ? (
+							<button onClick={() => signOut(auth)}>Sign Out</button>
+						) : (
+							<Link className='navbar-items' to='signin'>
+								Sign In
+							</Link>
+						)}
 					</li>
-				)}
-			</ul>
+					{isAdmin && (
+						<li className='hideSm'>
+							<Link className='navbar-items'>Admin</Link>
+						</li>
+					)}
+					<li className='moreWrap'>
+						<button
+							className='moreBtn'
+							onClick={() => setMoreOpen((v) => !v)}
+							aria-expanded={moreOpen}
+							aria-haspopup='menu'
+						>
+							More ▾
+						</button>
+						{moreOpen && (
+							<ul className='dropdown'>
+								<li>About Us</li>
+								<li>
+									{user ? (
+										<button onClick={() => signOut(auth)}>Sign Out</button>
+									) : (
+										<Link className='navbar-items' to='signin'>
+											Sign In
+										</Link>
+									)}
+								</li>
+								{isAdmin && (
+									<li>
+										<Link className='navbar-items'>Admin</Link>
+									</li>
+								)}
+							</ul>
+						)}
+					</li>
+				</ul>
+			</div>
 		</div>
 	);
 };
