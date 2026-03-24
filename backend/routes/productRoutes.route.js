03de85232bf493b542f08b3cd83f68d45d36d73e
CHUL CHIVORN <chivornchul2@gmail.com>
2026-03-24 17:19:08 +0700

    feat: Add API endpoints for product variant CRUD

--- backend/routes/productRoutes.route.js
+++ backend/routes/productRoutes.route.js
@@ -1,10 +1,13 @@
 import express from 'express';
 import {
+	addVariant,
 	createProduct,
 	deleteProduct,
+	deleteVariant,
 	getAllProducts,
 	getProductById,
 	updateProduct,
+	updateVariant,
 } from '../controllers/product.controller.js';
 
 const router = express.Router();
@@ -23,5 +26,13 @@ router.put('/:productId', updateProduct);
 
 //Delete product
 router.delete('/:productId', deleteProduct);
+
+//Variants
+//Add variant
+router.post('/:productId/variants/', addVariant);
+//Update variant
+router.put('/:productId/variants/:variantId', updateVariant);
+//Delete variant
+router.delete('/:productId/variants/:variantId', deleteVariant);
 
 export default router;
