99ee009c84b62502da1e8473908023837a298543
CHUL CHIVORN <chivornchul2@gmail.com>
2026-06-02 15:28:14 +0700

    Added favourites controller
    - created get, add, and remove favourites methods

---
+++ backend/controllers/favourites.controller.js
@@ -0,0 +1,55 @@
+import User from '../models/User';
+
+// --- Helper -------------------------------------------------------------
+export const findOrCreateUser = async ({ firebaseUid, email }) => {
+	const user = await User.findOne({ firebaseUid });
+	if (!user) {
+		user = await User.create({ firebaseUid, email });
+	}
+	return user;
+};
+
+// --- Get Favourites -----------------------------------------------------
+export const getFavourites = async (req, res) => {
+	try {
+		const user = await findOrCreateUser(req.user.uid, req.user.email);
+		await user.populate('favourites');
+		return res.status(200).json(user.favourites);
+	} catch (error) {
+		res.status(500).json({ message: 'Failed to get favourites', error });
+	}
+};
+
+// --- Add Favourites ----------------------------------------------------
+export const addFavourites = async (req, res) => {
+	try {
+		const user = await findOrCreateUser(req.user.uid, req.user.email);
+		const { productId } = req.params;
+		if (user.favourites.includes(productId)) {
+			return res.status(400).json({ message: 'Already in favourites.' });
+		}
+		user.favourites.push(productId);
+		await user.save();
+		await user.populate('favourites');
+		res.status(200).json(user.favourites);
+	} catch (error) {
+		res.status(500).json({ message: 'Failed to add favourite', error });
+	}
+};
+
+// --- Remove Favourites -------------------------------------------------
+export const removeFavourites = async (req, res) => {
+	try {
+		const user = await findOrCreateUser(req.user.uid, req.user.email);
+		const { productId } = req.params;
+		user.favourites = user.favourites.filter(
+			(id) => id.toString() !== productId,
+		);
+
+		await user.save();
+		await user.populate('favourites');
+		res.status(200).json(user.favourites);
+	} catch (error) {
+		res.status(500).json({ message: 'Failed to remove favourite', error });
+	}
+};
