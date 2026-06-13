2c069d3dbd1d79528ddcbb1ee6b131947dc64157
CHUL CHIVORN <chivornchul2@gmail.com>
2026-06-13 13:53:23 +0700

    Fix Link import

--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -1,4 +1,4 @@
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { useCart } from '../context/CartContext';
 import { useState } from 'react';
 import './Cart.css';
